82dc4b9e53c5a6e3bf655d9d6c29b87b8fa1875b
ebrahimsouissi <88770155+ebrahimsouissi@users.noreply.github.com>
2026-05-17 10:17:52 +0100

    Add headers for x-admin-password and x-slug-hint

--- worker-v2.js
+++ worker-v2.js
@@ -22,7 +22,7 @@
 const CORS = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization, x-admin-password, x-slug-hint",
 };
 
 function json(data, status = 200, extraHeaders = {}) {
